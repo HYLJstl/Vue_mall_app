82d3ab7c6a1623a83a80a170d5bdf695298fa0e6
root <1023400794@qq.com>
2020-01-29 19:55:54 +0800

    init project | 2020-01-29

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<main-tab-bar></main-tab-bar>
+	</div>
+</template>
+
+<script>
+    import MainTabBar from "@/components/MainTabBar";
+
+    export default {
+        name: 'App',
+        components: {
+            MainTabBar,
+        }
+    }
+</script>
+
+<style>
+	/*body {*/
+	/*	padding: 0px;*/
+	/*	margin: 0px;*/
+	/*}*/
+	
+	/*@import "src/assets/css/base.css";*/
+	
+	/*#app {*/
+	/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+	/*  -webkit-font-smoothing: antialiased;*/
+	/*  -moz-osx-font-smoothing: grayscale;*/
+	/*  text-align: center;*/
+	/*  color: #2c3e50;*/
+	/*}*/
+	
+	
+	/*#nav {*/
+	/*  padding: 30px;*/
+	/*}*/
+	
+	/*#nav a {*/
+	/*  font-weight: bold;*/
+	/*  color: #2c3e50;*/
+	/*}*/
+	
+	/*#nav a.router-link-exact-active {*/
+	/*  color: #42b983;*/
+	/*}*/
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h2>首页</h2>
+		<h3>counter : {{$store.state.counter}}</h3>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
